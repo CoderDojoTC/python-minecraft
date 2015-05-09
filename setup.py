b134675977d04eb6fa9865b81f68e5ded9c7803c
Mike McCallister <mike@mccllstr.com>
2015-05-09 00:27:43 -0500

    Added Martin O'Hanlon's Minecraft Turtles.
    Copied from commit 1875f7d5e90ef9a65d1fd32c93b1799366fb0129 of
    https://github.com/martinohanlon/minecraft-turtle.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mcpi',
+    name='coderdojo-python-minecraft',
     version='1.3',
     packages=find_packages(),
 
-    description='Minecraft PI low level api',
+    description='Python programming modules for Minecraft',
 
-    author='Aron Nieminen, Mojang AB',
+    author='Aron Nieminen, Mojang AB; Martin O\'Hanlon',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
