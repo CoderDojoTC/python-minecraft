a94d59a319833dbd58ae3eb2cad4cb6966d8ce3f
Mike McCallister <mike@mccllstr.com>
2015-05-08 22:01:01 -0500

    Upgraded to 1.3 version of MCPI code from Martin O'Hanlon.
    Copied from https://github.com/martinohanlon/CanaryRaspberryJuice

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mcpi',
-    version='0.1.1',
+    version='1.3',
     packages=find_packages(),
 
     description='Minecraft PI low level api',
