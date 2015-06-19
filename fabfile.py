227110a5e0df2b3bc3dea5a86ced378c5c014ae8
Mike McCallister <mike@mccllstr.com>
2015-06-18 23:12:01 -0500

    Enable task execution from any subdirectory, add virtualenv helper

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,9 @@ developing and delivering the course.
 Invoke it with the :command:`fab` command.
 """
 
+import os
+import os.path
+
 # TODO: Look at using docker-fabric in place of directly using
 # docker-py. It seems like it would help manage remote docker
 # instances through the Fabric SSH tunnels instead of needing to set
@@ -20,10 +23,24 @@ from fabric.colors import red
 # environment on AWS using boto.
 
 
+def topdir():
+    """Returns the directory where the fabfile is located"""
+    return os.path.dirname(env.real_fabfile)
+
+
+@task
+def virtualenv_install():
+    """Install/upgrade the virtual environment"""
+    if None == os.environ.get('VIRTUAL_ENV'):
+        abort(red("VIRTUAL_ENV variable missing from environment. Are you in an active virtualenv?"))
+    with lcd(topdir()):
+        local("pip install --upgrade -r requirements.txt")
+
+
 @task
 def docs_build():
     """Build a local copy of the documentation"""
-    with lcd('docs'):
+    with lcd(os.path.join(topdir(), 'docs')):
         local("make html")
 
 
@@ -35,7 +52,8 @@ def docs_show():
 
     # Launch a browser.
     # TODO: Add some code to do the right thing on operating systems other than Ubuntu/Debian
-    local("sensible-browser docs/_build/html/index.html")
+    with lcd(topdir()):
+        local("sensible-browser docs/_build/html/index.html")
 
 
 def student_env_container(base_url=None):
@@ -61,7 +79,8 @@ def env_sync():
 
     print(red("Use the Unison keyboard interface to specify which files to sync"))
     container = student_env_container()
-    local("docker exec -it {Id} sync-notebooks.sh".format(**container))
+    with lcd(topdir()):
+        local("docker exec -it {Id} sync-notebooks.sh".format(**container))
     # TODO: Would be nice to figure out why this returns an exit code
     # of 1, prompting warnings from local().
 
@@ -69,16 +88,19 @@ def env_sync():
 @task
 def env_up():
     """Start up the environment"""
-    local("vagrant up --provider=docker")
+    with lcd(topdir()):
+        local("vagrant up --provider=docker")
 
 
 @task
 def env_down():
     """Stop the environment"""
-    local("vagrant halt")
+    with lcd(topdir()):
+        local("vagrant halt")
 
 
 @task
 def env_destroy():
     """Destroy the environment"""
-    local("vagrant destroy")
+    with lcd(topdir()):
+        local("vagrant destroy")
